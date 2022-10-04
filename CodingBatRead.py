fcf51efe84e0b41cffe7d3e42a2358a9051579ed
thomas.kiesel@fwisd.org <thomas.kiesel@fwisd.org>
2022-10-04 09:41:05 -0500

    Removed license from .py file

--- CodingBatRead.py
+++ CodingBatRead.py
@@ -7,43 +7,14 @@ from datetime import date
 import requests
 from bs4 import BeautifulSoup
 
-'''
-This program was written for Python 3
-
-MIT License
-
-Copyright (c) 2020 Thomas Kiesel <thomas.j.kiesel@gmail.com>
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-'''
-
 ################################################################################
 # items the end user should edit!-----------------------------------------------
 ################################################################################
 
 # Login Credentials
+# See next variable for credentials file option.
 username = 'username'
 password = 'password'
-
-# Should the program print the names of students who haven't
-# finished any problems since the last grade pull?
-printNone = True
 
 # Should the program read the credentials from codingbat_auth.txt
 # instead of using the above values?
@@ -51,6 +22,10 @@ printNone = True
 #   username on first line
 #   password on second line
 readCreds = True
+
+# Should the program print the names of students who haven't
+# finished any problems since the last grade pull?
+printNone = True
 
 # TO DO
 # Should the program save a copy of the report in a text file?
